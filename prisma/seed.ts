1654d5b82afc3d0a2235b537158273b390597afa
busrauz78 <busrauz78@gmail.com>
2023-07-31 21:41:02 +0300

    added prisma and related integrations

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  // create two dummy articles
+  await prisma.article.createMany({
+    data: [
+      {
+        title: 'Prisma Adds Support for MongoDB',
+        body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
+        description:
+          "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
+        published: false,
+      },
+      {
+        title: "What's new in Prisma? (Q1/22)",
+        body: 'Our engineers have been working hard, issuing new releases with many improvements...',
+        description:
+          'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
+        published: true,
+      }
+    ],
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
